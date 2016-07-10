6d97ec1f3435430d127b8f01fe15827e02e421aa
Rustam <guseynov.r.v@gmail.com>
2016-07-10 19:38:12 +0000

    Handling errors

--- bot.py
+++ bot.py
@@ -7,7 +7,7 @@ import time
 bot = telebot.TeleBot(config.token)
 @bot.message_handler(commands=['photo'])
 def make_snapshot(message):
-	file_name = bot_utils.get_temp_file_name() + ".jpg"
+	file_name = "./snaps/" + bot_utils.get_temp_file_name() + ".jpg"
 	print("File is " + file_name)
 	temp_file = open(file_name, 'wb')
 	with picamera.PiCamera() as camera:
@@ -18,12 +18,32 @@ def make_snapshot(message):
 		temp_file.close()	
 	print("Sending...")
 	temp_file = open(file_name, 'rb')
-	bot.send_photo(message.chat.id, temp_file)
-	temp_file.close()	
+
+	try:
+		bot.send_photo(message.chat.id, temp_file)
+	except Exception as e:
+		print("Failed to send photo:" + e.strerror)
+	finally:
+		temp_file.close()	
+
+	try:
+		bot_utils.clear_folder("./snaps")
+	except Exception as e:
+		print("Failed to delete temp files: " + e.strerror)
+
 
 @bot.message_handler(content_types=["text"])
 def repeat_all_messages(message): 
+	print("Got message " + message.text)
 	bot.send_message(message.chat.id, message.text)
 
 if __name__ == '__main__':
-	bot.polling(none_stop=True)
+	while True:
+		try:
+			print("Connecting...")
+			bot.polling(none_stop=True)
+		except Exception as e:
+			print("Some polling error happened: " + e.strerror)
+			print("Will try to reconnect in 10 seconds")
+			time.sleep(10)
+		
--- bot_utils.py
+++ bot_utils.py
@@ -1,5 +1,16 @@
 import time
 import random
+import os
+import shutil
 
 def get_temp_file_name(): 
 	return str(random.randint(10000, 99999)) + "_" + str(int(round(time.time() * 1000)))
+
+def clear_folder(folder):
+	for the_file in os.listdir(folder):
+		file_path = os.path.join(folder, the_file)
+		try:
+			if os.path.isfile(file_path):
+				os.unlink(file_path)
+		except Exception as e:
+			print(e)
