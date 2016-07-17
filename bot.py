5d0762e4f6b72976a9e757b92f000dd9c30c4f3b
Rustam <guseynov.r.v@gmail.com>
2016-07-17 15:27:23 +0000

    Fixed logging and fixed exception handling

--- bot.py
+++ bot.py
@@ -4,13 +4,21 @@ import bot_utils
 import picamera
 import time
 import logging
+import sys
+import signal
+
+def ctrlchandler(signum, frame):
+    print("Bye!")
+    sys.exit()
+
+signal.signal(signal.SIGINT, ctrlchandler)
 
 logger = telebot.logger
 telebot.logger.setLevel(logging.DEBUG)
 
 def make_and_save_snapshot():
     file_name = "./snaps/" + bot_utils.get_temp_file_name() + ".jpg"
-    logging.debug("File is %s", file_name)
+    print("File is %s" % file_name)
     temp_file = open(file_name, 'wb')
     with picamera.PiCamera() as camera:
         camera.resolution = (640, 480)
@@ -29,36 +37,33 @@ def inline_handler(query):
 
 @bot.message_handler(commands=['photo'])
 def make_snapshot(message):
-    logging.debug("User %s requested a photo", message.from_user.first_name)
+    print("User %s requested a photo" % message.from_user.first_name)
     file_name = make_and_save_snapshot()
     temp_file = open(file_name, 'rb')
     try:
-        logging.debug("Sending...")
+        print("Sending...")
         bot.send_photo(message.chat.id, temp_file)
     except Exception as e:
-        logging.error("Failed to send photo", exc_info=e)
+        print("Failed to send photo: {0}".format(e))
     finally:
         temp_file.close()
 
     try:
         bot_utils.clear_folder("./snaps")
     except Exception as e:
-        logging.error("Failed to delete temp files", exc_info=e)
-
+        print("Failed to delete temp files: {0}".format(e))
 
 @bot.message_handler(content_types=["text"])
 def repeat_all_messages(message):
-    print("Got message %s from %s", message.text, message.from_user.first_name)
+    print("Got message %s from %s" % (message.text, message.from_user.first_name))
     bot.send_message(message.chat.id, message.text)
 
 if __name__ == '__main__':
-    try:
-        logging.info("Connecting...")
-        bot.polling(none_stop=False)
-    except KeyboardInterrupt:
-        logging.ingo("Bye!")
-        interrupted = True
-    except Exception as e:
-        logging.error("Some polling error happened", exc_info=e)
-        logging.info("Will try to reconnect in 10 seconds")
-        time.sleep(10)
+    while True:
+        print("Connecting...")
+        try:
+            bot.polling(none_stop=False)
+        except Exception as e:
+            print("Some polling error happened: {0}".format(e))
+            print("Will try to reconnect in 10 seconds")
+            time.sleep(10)
