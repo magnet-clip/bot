d51013e66c830e31edcb6c3cbad478d03015c653
rustam.guseynov <rustam.guseynov@thomsonreuters.com>
2016-07-27 11:43:09 +0300

    Added few tests on setting boss

--- test/events.py
+++ test/events.py
@@ -5,11 +5,12 @@ from handler import Handler
 from confmanager import ConfManager
 from dictate import Dictate
 
+
 class EventsTest(unittest.TestCase):
     def tearDown(self):
         os.unlink('./temp.config')
 
-    def test_simple(self):
+    def test_set_new_boss(self):
         bot = Mock()
         man = ConfManager('temp.config')
         cam = Mock()
@@ -27,6 +28,60 @@ class EventsTest(unittest.TestCase):
 
         handler.set_boss(message)
         self.assertTrue(bot.send_message.called)
+        bot.send_message.assert_called_with('1', "You are the king now")
+
+    def test_set_new_boss_twice(self):
+        bot = Mock()
+        man = ConfManager('temp.config')
+        cam = Mock()
+        handler = Handler(bot, man, cam)
+
+        message = Dictate({
+            'from_user': {
+                'first_name': 'R',
+                'id': '12'
+            },
+            'chat': {
+                'id': '1'
+            }
+        })
+
+        handler.set_boss(message)
+        self.assertTrue(bot.send_message.called)
+        handler.set_boss(message)
+        self.assertEqual(bot.send_message.call_count, 2)
+        bot.send_message.assert_called_with('1', "I know")
+
+    def test_set_new_boss_when_one_exists(self):
+        bot = Mock()
+        man = ConfManager('temp.config')
+        cam = Mock()
+        handler = Handler(bot, man, cam)
+
+        message = Dictate({
+            'from_user': {
+                'first_name': 'R',
+                'id': '12'
+            },
+            'chat': {
+                'id': '1'
+            }
+        })
+
+        handler.set_boss(message)
+
+        message = Dictate({
+            'from_user': {
+                'first_name': 'L',
+                'id': '13'
+            },
+            'chat': {
+                'id': '2'
+            }
+        })
+        handler.set_boss(message)
+        self.assertEqual(bot.send_message.call_count, 2)
+        bot.send_message.assert_called_with('2', "Nope")
 
 
 if __name__ == '__main__':
