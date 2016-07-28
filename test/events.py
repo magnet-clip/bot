dad4a128ba363544c59b23fc2cce0ab4c90d1b27
rustam.guseynov <rustam.guseynov@thomsonreuters.com>
2016-07-28 07:12:04 +0300

    making tests smaller

--- test/events.py
+++ test/events.py
@@ -6,190 +6,110 @@ from confmanager import ConfManager
 from dictate import Dictate
 
 
+def create_objects():
+    bot = Mock()
+    man = ConfManager('temp.config')
+    cam = Mock()
+
+    handler = Handler(bot, man, cam)
+    return handler, bot, man, cam
+
+
+def create_message(id, chat_id, name, username="", text=""):
+    return Dictate({
+        'from_user': {
+            'first_name': name,
+            'username': username,
+            'id': id
+        },
+        'chat': {
+            'id': chat_id
+        },
+        text: text
+    })
+
+
 class EventsTest(unittest.TestCase):
-    def create_objects(self):
-        bot = Mock()
-        man = ConfManager('temp.config')
-        cam = Mock()
-
-        handler = Handler(bot, man, cam)
-        return handler, bot, man, cam
-
     def tearDown(self):
         os.unlink('./temp.config')
 
     def test_set_new_boss(self):
-        handler, bot, man, cam = self.create_objects()
+        handler, bot, man, cam = create_objects()
 
-        message = Dictate({
-            'from_user': {
-                'first_name': 'R',
-                'id': '12'
-            },
-            'chat': {
-                'id': '1'
-            }
-        })
-
+        message = create_message(id='12', name='R', chat_id='1')
         handler.set_boss(message)
         self.assertTrue(bot.send_message.called)
         bot.send_message.assert_called_with('1', "You are the king now")
 
     def test_set_new_boss_twice(self):
-        handler, bot, man, cam = self.create_objects()
+        handler, bot, man, cam = create_objects()
 
-        message = Dictate({
-            'from_user': {
-                'first_name': 'R',
-                'id': '12'
-            },
-            'chat': {
-                'id': '1'
-            }
-        })
-
+        message = create_message(id='12', name='R', chat_id='1')
         handler.set_boss(message)
         self.assertTrue(bot.send_message.called)
+
+        message = create_message(id='12', name='R', chat_id='1')
         handler.set_boss(message)
         self.assertEqual(bot.send_message.call_count, 2)
         bot.send_message.assert_called_with('1', "I know")
 
     def test_set_new_boss_when_one_exists(self):
-        handler, bot, man, cam = self.create_objects()
+        handler, bot, man, cam = create_objects()
 
-        message = Dictate({
-            'from_user': {
-                'first_name': 'R',
-                'id': '12'
-            },
-            'chat': {
-                'id': '1'
-            }
-        })
-
+        message = create_message(id='12', name='R', chat_id='1')
         handler.set_boss(message)
 
-        message = Dictate({
-            'from_user': {
-                'first_name': 'L',
-                'id': '13'
-            },
-            'chat': {
-                'id': '2'
-            }
-        })
+        message = create_message(id='13', name='L', chat_id='2')
         handler.set_boss(message)
         self.assertEqual(bot.send_message.call_count, 2)
         bot.send_message.assert_called_with('2', "Nope")
 
     def test_set_boss_and_resign(self):
-        handler, bot, man, cam = self.create_objects()
+        handler, bot, man, cam = create_objects()
 
-        message = Dictate({
-            'from_user': {
-                'first_name': 'R',
-                'id': '12'
-            },
-            'chat': {
-                'id': '1'
-            }
-        })
-
+        message = create_message(id='12', name='R', chat_id='1')
         handler.set_boss(message)
         self.assertTrue(man.has_super_user())
 
-        message = Dictate({
-            'from_user': {
-                'first_name': 'R',
-                'id': '12'
-            },
-            'chat': {
-                'id': '1'
-            }
-        })
         handler.resign(message)
         self.assertEqual(bot.send_message.call_count, 2)
         bot.send_message.assert_called_with('1', "We have a vacancy now")
         self.assertFalse(man.has_super_user())
 
     def test_set_boss_and_resign_from_other(self):
-        handler, bot, man, cam = self.create_objects()
+        handler, bot, man, cam = create_objects()
 
-        message = Dictate({
-            'from_user': {
-                'first_name': 'R',
-                'id': '12'
-            },
-            'chat': {
-                'id': '1'
-            }
-        })
-
+        message = create_message(id='12', name='R', chat_id='1')
         handler.set_boss(message)
 
-        message = Dictate({
-            'from_user': {
-                'first_name': 'L',
-                'id': '13'
-            },
-            'chat': {
-                'id': '2'
-            }
-        })
+        message = create_message(id='13', name='L', chat_id='2')
         handler.resign(message)
         self.assertEqual(bot.send_message.call_count, 1)
         self.assertTrue(man.has_super_user())
         self.assertEqual(man.get_admin_id(), '12')
 
     def test_simple_grant_access_request(self):
-        handler, bot, man, cam = self.create_objects()
+        handler, bot, man, cam = create_objects()
 
         # set admin
-        message = Dictate({
-            'from_user': {
-                'first_name': 'R',
-                'id': '12'
-            },
-            'chat': {
-                'id': '1'
-            }
-        })
+        message = create_message(id='12', name='R', chat_id='1')
         handler.set_boss(message)
 
         # request access
-        message = Dictate({
-            'from_user': {
-                'username': 'Vova',
-                'first_name': 'L',
-                'id': '13'
-            },
-            'chat': {
-                'id': '2'
-            },
-            'text': '/getaccess'
-        })
+        message = create_message(id='13', name='L', username='Vova', chat_id='2', text="/getaccess")
         handler.get_access(message)
         bot.send_message.assert_called_with('12', "User L id [13] wants to get access; Type /grant13 to allow, /ban13 to ban him")
         bot.send_message.assert_any_call('13', "Your application is under review")
         self.assertEqual(bot.send_message.call_count, 3)
 
         # and grant access
-        message = Dictate({
-            'from_user': {
-                'first_name': 'R',
-                'id': '12'
-            },
-            'chat': {
-                'id': '2'
-            },
-            'text': '/grant13'
-        })
+        message = create_message(id='12', name='R', chat_id='1', text="/grant13")
         handler.grant_access(message, '13')
         bot.send_message.assert_called_with('13', "Willkommen!")
         self.assertEqual(bot.send_message.call_count, 4)
 
     def test_wrong_grant_command(self):
-        handler, bot, man, cam = self.create_objects()
+        handler, bot, man, cam = create_objects()
 
         # set admin
         message = Dictate({
@@ -219,7 +139,7 @@ class EventsTest(unittest.TestCase):
         self.assertEqual(bot.send_message.call_count, 2)
 
     def test_wrong_grant_access_nouser(self):
-        handler, bot, man, cam = self.create_objects()
+        handler, bot, man, cam = create_objects()
 
         # set admin
         message = Dictate({
@@ -248,7 +168,7 @@ class EventsTest(unittest.TestCase):
         bot.send_message.assert_called_with('12', "Failed to grant access")
 
     def test_unauthorized_user_takes_photo(self):
-        handler, bot, man, cam = self.create_objects()
+        handler, bot, man, cam = create_objects()
 
         message = Dictate({
             'from_user': {
@@ -266,7 +186,7 @@ class EventsTest(unittest.TestCase):
 
 
     def test_authorized_user_takes_photo(self):
-        handler, bot, man, cam = self.create_objects()
+        handler, bot, man, cam = create_objects()
 
         message = Dictate({
             'from_user': {
