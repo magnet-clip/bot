4f76f0249f0b89eceea6b756c87ecace072a4c56
rustam.guseynov <rustam.guseynov@thomsonreuters.com>
2016-08-09 14:09:16 +0300

    fix

--- helper.py
+++ helper.py
@@ -1,23 +1,4 @@
-import pygal
-from datetime import datetime
 import os
-
-
-def create_picture(items, field):
-    print("Creating chart...")
-    try:
-        chart = pygal.Line()
-        times = map(datetime.fromtimestamp, [item['time'] for item in items])
-        times = map(lambda d: d.strftime('%H:%M:%S'), times)
-        chart.x_labels = list(map(str, times))
-
-        values = list(map(lambda x: int(x[0]), [item[field] for item in items]))
-        chart.add(field, values)
-
-        chart.render_to_file("./test.svg")
-        chart.render_to_png("./test.png")
-    except Exception as e:
-        print(e)
 
 
 def clear_folder(folder):
